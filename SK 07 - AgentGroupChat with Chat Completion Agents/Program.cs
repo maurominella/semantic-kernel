bc0d6f0093bfaa7c671d721bf7ca953f825571e9
Mauro Minella <mauro.minella@mngenv789911.onmicrosoft.com>
2025-02-08 00:03:56 +0100

    sk

--- SK 07 - AgentGroupChat with Chat Completion Agents/Program.cs
+++ SK 07 - AgentGroupChat with Chat Completion Agents/Program.cs
@@ -35,67 +35,29 @@ internal class Program
         // Add enterprise logging components
         builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
 
-        // Create agent code_validator
-        string codevalidator_agent_name = "CodeValidator";
-        string codevalidator_agent_instruction = ReadAgentInstructions(codevalidator_agent_name);
-        var codevalidator_agent = CreateAgent(name: codevalidator_agent_name, instructions: codevalidator_agent_instruction, kernel: kernel);
+        // Create the three agents
+        var codevalidator_agent = CreateAgent(agent_name: "CodeValidator", kernel: kernel);
+        var juniordeveloper_agent = CreateAgent(agent_name: "JuniorDeveloper", kernel: kernel);
+        var seniordeveloper_agent = CreateAgent(agent_name: "SeniorDeveloper", kernel: kernel);
 
-        // Create agent junior_developer
-        string juniordeveloper_agent_name = "JuniorDeveloper";
-        string juniordeveloper_agent_instruction = ReadAgentInstructions(juniordeveloper_agent_name);
-        var juniordeveloper_agent = CreateAgent(name: juniordeveloper_agent_name, instructions: juniordeveloper_agent_instruction, kernel: kernel);
-
-        // Create agent senior_developer
-        string seniordeveloper_agent_name = "SeniorDeveloper";
-        string seniordeveloper_agent_instruction = ReadAgentInstructions(seniordeveloper_agent_name);
-        var seniordeveloper_agent = CreateAgent(name: seniordeveloper_agent_name, instructions: seniordeveloper_agent_instruction, kernel: kernel);
-
-        // termination function
-        var terminate_function = KernelFunctionFactory.CreateFromPrompt(
-            $$$"""
-            Determine if the code has been approved. If so, respond with a single word: yes.
-
-            History:
-            {{$history}}
-            """
-        );
-
-        // termination function
-        var selection_function = KernelFunctionFactory.CreateFromPrompt(
-            $$$"""
-            Your job is to determine which agent takes the next turn in a conversation.
-            State only the name of the participant to take the next turn.
-            Choose only from these participants:
-            - {{{codevalidator_agent_name}}}.
-            - {{{juniordeveloper_agent_name}}}.
-            - {{{seniordeveloper_agent_name}}}.
-
-            Follow these rules:
-            1) After the user input, it is {{{juniordeveloper_agent_name}}} turn.
-            2) After {{{juniordeveloper_agent_name}}}, it's {{{codevalidator_agent_name}}}'s turn.
-            3) if the score provided by {{{codevalidator_agent_name}}} is less than 8, it's the {{{seniordeveloper_agent_name}}}'s turn.
-            4) After {{{seniordeveloper_agent_name}}} replies, it's {{{codevalidator_agent_name}}}'s turn to validate the code.
-            5) Repeat step 3 and 4 until the code is approved.
-
-            History:
-            {{$history}}
-            """
-        );
 
         var chat = new AgentGroupChat(codevalidator_agent, juniordeveloper_agent, seniordeveloper_agent)
         {
             ExecutionSettings = new()
             {
-                TerminationStrategy = termination_strategy(
-                    terminate_function: terminate_function,
-                    kernel: kernel,
-                    agents: [codevalidator_agent]
-                ),
+                TerminationStrategy = new KernelFunctionTerminationStrategy(function: terminate_function(), kernel: kernel)
+                {
+                    Agents = [codevalidator_agent],
+                    ResultParser = (result) => result.GetValue<string>()?.Contains("yes", StringComparison.OrdinalIgnoreCase) ?? false,
+                    HistoryVariableName = "history",
+                    MaximumIterations = 10
+                },
 
-                SelectionStrategy = selection_strategy(
-                    selection_function: selection_function,
-                    kernel: kernel
-                )
+                SelectionStrategy = new KernelFunctionSelectionStrategy(function: selection_function("CodeValidator", "JuniorDeveloper", "SeniorDeveloper"), kernel: kernel)
+                {
+                    //AgentsVariableName = "agents",
+                    HistoryVariableName = "history"
+                }
             }
         };
 
@@ -118,12 +80,12 @@ internal class Program
     }
 
     // Helper function to create an agent
-    private static ChatCompletionAgent CreateAgent(string name, string instructions, Kernel kernel)
+    private static ChatCompletionAgent CreateAgent(string agent_name, Kernel kernel)
     {
         return new ChatCompletionAgent
         {
-            Name = name,
-            Instructions = instructions,
+            Name = agent_name,
+            Instructions = ReadAgentInstructions(agent_name),
             Kernel = kernel
         };
     }
@@ -132,7 +94,7 @@ internal class Program
     {
         return KernelFunctionFactory.CreateFromPrompt(
             $$$"""
-            Determine if the ode has been approved. If so, respond with a single word: yes.
+            Determine if the code has been approved. If so, respond with a single word: yes.
 
             History:
             {{$history}}
@@ -140,22 +102,27 @@ internal class Program
         );
     }
 
-    private static KernelFunctionTerminationStrategy termination_strategy(KernelFunction terminate_function, Kernel kernel, ChatCompletionAgent[] agents)
+    private static KernelFunction selection_function(string codevalidator_agent_name, string juniordeveloper_agent_name, string seniordeveloper_agent_name)
     {
-        return new KernelFunctionTerminationStrategy(function: terminate_function, kernel: kernel)
-        {
-            Agents = agents,
-            ResultParser = (result) => result.GetValue<string>()?.Contains("yes", StringComparison.OrdinalIgnoreCase) ?? false,
-            HistoryVariableName = "history",
-            MaximumIterations = 10
-        };
-    }
-    private static KernelFunctionSelectionStrategy selection_strategy(KernelFunction selection_function, Kernel kernel)
-    {
-        return new KernelFunctionSelectionStrategy(function: selection_function, kernel: kernel)
-        {
-            AgentsVariableName = "agents",
-            HistoryVariableName = "history"
-        };
+        return KernelFunctionFactory.CreateFromPrompt(
+            $$$"""
+            Your job is to determine which agent takes the next turn in a conversation.
+            State only the name of the participant to take the next turn.
+            Choose only from these participants:
+            - {{{codevalidator_agent_name}}}.
+            - {{{juniordeveloper_agent_name}}}.
+            - {{{seniordeveloper_agent_name}}}.
+
+            Follow these rules:
+            1) After the user input, it is {{{juniordeveloper_agent_name}}} turn.
+            2) After {{{juniordeveloper_agent_name}}}, it's {{{codevalidator_agent_name}}}'s turn.
+            3) if the score provided by {{{codevalidator_agent_name}}} is less than 8, it's the {{{seniordeveloper_agent_name}}}'s turn.
+            4) After {{{seniordeveloper_agent_name}}} replies, it's {{{codevalidator_agent_name}}}'s turn to validate the code.
+            5) Repeat step 3 and 4 until the code is approved.
+
+            History:
+            {{$history}}
+            """
+        );
     }
 }
