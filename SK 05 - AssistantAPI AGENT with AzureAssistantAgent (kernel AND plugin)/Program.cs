349b4324f0aad7b05b8d50731525b1262f8f5c0f
Mauro Minella <mauro.minella@mngenv789911.onmicrosoft.com>
2025-04-01 01:11:18 +0200

    up to SK 05 in both Python and C#

--- SK 05 - AssistantAPI AGENT with AzureAssistantAgent (kernel AND plugin)/Program.cs
+++ SK 05 - AssistantAPI AGENT with AzureAssistantAgent (kernel AND plugin)/Program.cs
@@ -1,17 +1,32 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+// Last update: March 31st, 2025
+
 // See https://aka.ms/new-console-template for more information
 
-// Import packages: 
-// https://learn.microsoft.com/en-us/semantic-kernel/frameworks/agent/examples/example-assistant-code?pivots=programming-language-csharp
+// OpenAIAssistantAgent is the KEY of this exercise 
+// Migration guide: https://learn.microsoft.com/en-us/semantic-kernel/support/migration/agent-framework-rc-migration-guide?pivots=programming-language-csharp
+// Docs: https://learn.microsoft.com/en-us/semantic-kernel/frameworks/agent/assistant-agent?pivots=programming-language-csharp
+// Class: https://learn.microsoft.com/en-us/dotnet/api/microsoft.semantickernel.agents.openai.openaiassistantagent?view=semantic-kernel-dotnet
 
 using DotNetEnv;
 using MyApp.Plugins;
+
+// dotnet add package Azure.Identity --> <PackageReference Include="Azure.Identity" Version="1.13.2" />
 using Azure.Identity;
 
+// dotnet add package Microsoft.SemanticKernel --> <PackageReference Include="Microsoft.SemanticKernel" Version="1.44.0" />
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+
+// dotnet add package Microsoft.SemanticKernel.Agents.OpenAI --prerelease --> <PackageReference Include="Microsoft.SemanticKernel.Agents.AzureAI" Version="1.44.0-preview" />
 using Microsoft.SemanticKernel.Agents.OpenAI;
+using Azure.AI.OpenAI;
+using OpenAI.Assistants;
+using OpenAI.Files;
+
+// dotnet add package Microsoft.SemanticKernel.Agents.OpenAI --> 
+
 
 #pragma warning disable SKEXP0110 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
@@ -43,49 +58,46 @@ internal class Program
         // Load the environment variables from the .env file
         Env.Load(envFilePath);
 
+        // Instantiation of the Client for Azure OpenAI 
+        AzureOpenAIClient openaiClient = OpenAIAssistantAgent.CreateAzureOpenAIClient(
+            new AzureCliCredential(), new Uri(Env.GetString("AZURE_OPENAI_ENDPOINT")));
+
+        // just for testing: upload a file
+        OpenAIFileClient fileClient = openaiClient.GetOpenAIFileClient();
+        OpenAIFile fileInfo = await fileClient.UploadFileAsync("product_info_1.md", FileUploadPurpose.Assistants);
+
+#pragma warning disable OPENAI001 // 'OpenAI.Assistants.AssistantClient' is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        // Using the Azure OpenAI Client, now extract another Client for OpenAI Assistant Agent
+        AssistantClient assistantClient = openaiClient.GetAssistantClient();
+#pragma warning restore OPENAI001
+
+        // using the Client for OpenAI Assistant Agent, now either...
+        // ...EXTRACT the definition for a specific EXISTING OpenAI Assistant:
+        // var assistantDefinition = await assistantClient.GetAssistantAsync(assistantId: "");
+
+        //... or CREATE the definition for a NEW OpenAI Assistant:
+        string agent_name = "mauromi_assistant_agent_c#";
+        string instructions = "you are a clever agent";
+
+        var assistantDefinition = await assistantClient.CreateAssistantAsync(
+            modelId: Env.GetString("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"),
+            name: agent_name,
+            instructions: instructions,
+            enableCodeInterpreter: true
+        );
+
+        KernelPlugin lightsPlugin = KernelPluginFactory.CreateFromType<LightsPlugin>();
+
+        // using the definition of a specific (new or existing) OpenAI Assistant, now we may directly instantiate an OpenAIAssistantAgent 
+        var agent = new OpenAIAssistantAgent(definition: assistantDefinition, client: assistantClient, plugins: [lightsPlugin]);
+
+
         Console.WriteLine($"AZURE_OPENAI_ENDPOINT: {Env.GetString("AZURE_OPENAI_ENDPOINT")}\n" +
-        $"AZURE_OPENAI_CHAT_DEPLOYMENT_NAME: {Env.GetString("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME")}");
+            $"AZURE_OPENAI_CHAT_DEPLOYMENT_NAME: {Env.GetString("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME")}");
 
         // Add enterprise logging components
         // TBI
 
-        // Build the EMPTY kernel
-        var kernel = new Kernel();
-
-        // Add a plugin to the kernel (the LightsPlugin class is defined in its dedicated file LightsPlugin.cs)
-        kernel.Plugins.AddFromType<LightsPlugin>("Lights");
-
-        // Enabling planning with AzureOpenAIPromptExecutionSettings seems that is not needed here
-
-        // Create the OpenAI Assistant Agent
-        Console.WriteLine("\nDefining Assistant Agent...");
-        string agent_name = "mauromi_assistant_agent_c#";
-        string instructions = "you are a clever agent";
-
-        // OpenAIClientProvider will be used for the Agent Definition as well as file-upload
-        var clientProviderForAzure = OpenAIClientProvider.ForAzureOpenAI(
-            credential: new AzureCliCredential(),
-            endpoint: new Uri(Env.GetString("AZURE_OPENAI_ENDPOINT")));
-
-        // Create Open AI Assistant Agent
-        var agent =
-            await OpenAIAssistantAgent.CreateAsync(
-                clientProvider: clientProviderForAzure,
-                definition: new OpenAIAssistantDefinition(Env.GetString("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"))
-                {
-                    Name = agent_name,
-                    Instructions = instructions,
-                    EnableCodeInterpreter = false,
-                    EnableFileSearch = false
-                },
-                kernel: kernel // just with its plugin(s), no other serivices
-                );
-
-        Console.WriteLine("...Assistant Agent is ready.");
-
-        Console.WriteLine("\nCreating thread...");
-        string threadId = await agent.CreateThreadAsync();
-        Console.WriteLine("...thread was created.\n");
 
         // Initiate a back-and-forth chat
         bool isComplete = false;
@@ -108,11 +120,11 @@ internal class Program
                     break;
                 }
 
-                await agent.AddChatMessageAsync(threadId, new ChatMessageContent(AuthorRole.User, userInput));
+                var message = new ChatMessageContent(AuthorRole.User, userInput);
 
                 try
                 {
-                    await foreach (StreamingChatMessageContent response in agent.InvokeStreamingAsync(threadId: threadId))
+                    await foreach (StreamingChatMessageContent response in agent.InvokeStreamingAsync(message: message))
                     {
                         // Display response.
                         Console.Write($"{response.Content}");
@@ -134,10 +146,17 @@ internal class Program
         {
             Console.WriteLine();
             Console.WriteLine("Cleaning-up...");
+            var file_list = await fileClient.GetFilesAsync();
+            foreach (var f in file_list.Value)
+            {
+                Console.WriteLine($"Deleting file {f.Filename} ({f.Id})...");
+                await fileClient.DeleteFileAsync(f.Id);
+            }
+
+            Console.WriteLine($"Deleting assistant {agent.Name} ({agent.Id})...");
             await Task.WhenAll(
                 [
-                    agent.DeleteThreadAsync(threadId),
-                    agent.DeleteAsync()
+                    assistantClient.DeleteAssistantAsync(agent.Id),
                 ]);
         }
 
